be2ec85406ab29b7a1cebec8ade8deb43676b7af
TalmonG <118438762+TalmonG@users.noreply.github.com>
2023-01-16 10:39:38 +0000

    ggg

---
+++ Assets/Scripts/ClickAnimation.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickAnimation : MonoBehaviour
+{
+    public GameObject coin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Shoots a coin when click Fire1 (left click)
+    public void ClickCoinAnimation()
+    {
+        Debug.Log("hi");
+        GameObject newCoin = Instantiate (coin, transform.position, transform.rotation);
+        newCoin.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform,false);
+        Destroy(newCoin, 3.0f);
+        
+    }
+
+    
+    
+}
